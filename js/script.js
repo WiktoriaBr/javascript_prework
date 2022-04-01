4ff9d916f24f6900ec330e83e2f069fec8af4c64
Wiktoria Bryniowska <w.bryniowska@gmail.com>
2022-04-01 23:48:22 +0200

    Fix reading of player's move

--- js/script.js
+++ js/script.js
@@ -26,14 +26,15 @@ let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.
 
 console.log('Gracz wpisał: ' + playerInput);
 
-if(playerInput == '1'){
+if (playerInput == '1')
+{
     playerMove = 'kamień';
 }
-else if (randomNumber == 2)
+else if (playerInput == 2)
 {
     playerMove = 'papier';
 }
-else if (randomNumber == 3)
+else if (playerInput == 3)
 {
     playerMove = 'nożyce';
 }
@@ -42,3 +43,8 @@ else
     playerMove = 'nieznany ruch';
 }
 printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier')
+{
+    printMessage('Ty wygrywasz!');
+}
