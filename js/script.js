15d8b54a4affa1e8ab2d67c48adff1880f809742
Wiktoria Bryniowska <w.bryniowska@gmail.com>
2022-04-01 23:34:37 +0200

    Add reading of player's move

--- js/script.js
+++ js/script.js
@@ -1,11 +1,12 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 let computerMove = 'nieznany ruch';
+let playerMove = 'nieznany ruch';
 
 console.log('Wylosowana liczba to: ' + randomNumber);
 
 if(randomNumber == 1)
 {
-  computerMove = 'kamień';
+    computerMove = 'kamień';
 }
 else if (randomNumber == 2)
 {
@@ -19,6 +20,25 @@ else
 {
     computerMove = 'nieznany ruch';
 }
-
 printMessage('Mój ruch to: ' + computerMove);
 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+if(playerInput == '1'){
+    playerMove = 'kamień';
+}
+else if (randomNumber == 2)
+{
+    playerMove = 'papier';
+}
+else if (randomNumber == 3)
+{
+    playerMove = 'nożyce';
+}
+else
+{
+    playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch to: ' + playerMove);
