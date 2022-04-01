48e098d6e4c85c7ff099cf762a3e507c08e518ea
Wiktoria Bryniowska <w.bryniowska@gmail.com>
2022-04-01 23:31:07 +0200

    Add reading of the computer's move

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
-let computerMove = `kamień`;
-let playerMove = `papier`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!!!');
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = 'nieznany ruch';
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if(randomNumber == 1)
+{
+  computerMove = 'kamień';
+}
+else if (randomNumber == 2)
+{
+    computerMove = 'papier';
+}
+else if (randomNumber == 3)
+{
+    computerMove = 'nożyce';
+}
+else
+{
+    computerMove = 'nieznany ruch';
+}
+
+printMessage('Mój ruch to: ' + computerMove);
+
